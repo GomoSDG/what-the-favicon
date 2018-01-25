92270aa409d661bb7b355dbb43e482b21b6d0578
gomosdg <lilokoego@outlook.com>
2018-01-25 12:22:47 +0200

    fix width

--- js/movements.js
+++ js/movements.js
@@ -6,10 +6,14 @@ function LeftMovement(){
 
 function SineMovement(){
     this.x = 0;
-    this.increase = Math.PI / 480;
+    this.increase = Math.PI / (480 / 2);
     this.peak = 2;
 
     this.move = function(moveObject){
+        if(moveObject.x > 480 ){
+            this.x =  this.increase;
+            moveObject.x = -moveObject.width;
+        }
         moveObject.x++;
         moveObject.y = (this.peak / 2 - (Math.sin(this.x) * this.peak / 2)) - moveObject.height / 2;
         this.x += this.increase;
