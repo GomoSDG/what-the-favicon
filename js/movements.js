3a3c5428ad01a21feea0fb7e24d479e697761939
gomosdg <lilokoego@outlook.com>
2018-01-26 19:07:30 +0200

    update movements

--- js/movements.js
+++ js/movements.js
@@ -1,24 +1,40 @@
-function LeftMovement(){
-    this.move = function(moveObject){
-        moveObject.x += 1;
+function RightMovement(width){
+    this.width
+    this.move = function(obj){
+        if(obj.x > width ){
+            obj.x = -obj.width;
+        }
+
+        obj.x += 1;
     }
 }
 
 function SineMovement(width, peak){
     this.x = 0;
     this.width = width;
-    this.increase = Math.PI / (width / 2);
-    console.log(width);
+    this.pf = Math.PI / (width / 2); //Periodicity Factor
     this.peak = peak;
 
-    this.move = function(moveObject){
-        if(moveObject.x > width ){
-            this.x =  -moveObject.width * this.increase;
-            moveObject.x = -moveObject.width;
+    this.move = function(obj){
+        if(obj.x > width ){
+            this.x =  -obj.width * this.increase;
+            obj.x = -obj.width;
         }
         
-        moveObject.x++;
-        moveObject.y = (this.peak / 2 - (Math.sin(this.x) * this.peak / 2)) - moveObject.height / 2;
+        obj.x++;
+        //moveObject.y = (this.peak / 2 - (Math.sin(this.x) * this.peak / 2)) - moveObject.height / 2;
+        obj.y = (this.peak / 2 - (sine(this.peak / 2, this.pf, obj.x, 0, 0))) - obj.height / 2;
         this.x += this.increase;
     }
+}
+
+function SideToSideMovement(){
+    this.direction
+    this.move = function(obj){
+
+    }
+}
+
+function LeftOfSineMovement(){
+
 }
